11a39be50690f205814eaa9f867ea14bb1147577
SarayWHJ <77762804+SarayWHJ@users.noreply.github.com>
2021-07-13 20:27:13 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+/*
+1 cup of milk
+1 teaspoon of apple cider vinegar
+1/2 cup of margarine or butter
+1 cup of brown sugar
+2 tablespoons oof molasses
+1 teaspoon of vanilla extract
+1-1/2 cups of all-purpose flour
+1 teaspoon of backing powder
+1/2 teaspoon of baking soda
+1/2 teaspoon of salt
+1/2 cup of chocolate chips
+
+1. preheat oven to 350 degrees F.
+2. in a small bowl, mix the mil and vinegar together and set aside to curddle for a few minutes.
+3. in a large bowl, using a handheld mixer, cream the magarine or butter for a few minutes until lighter in 
+color, then add the brown sugar and mix thoroughly. Add the curddled milk, molasses, and vanilla extract.
+4. in another bowl, sift together the flour, baking powder, baking soda, salt, and then mix.
+5. add the dry ingredients to the wet ones a little bit at a time, and combine using a whisk or a handheld
+mixer until there sare no lumps lefts. Add the chocolate chips and combine completely.
+6. fill cupcake liners two thirds of the way and bake for 20-22 minutes.
+
+*/
